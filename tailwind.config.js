2636223771cda3e36465b1cd1fb9fc9114fffebc
Sri Agung Tirtayasa <agungjordan.aj@gmail.com>
2022-11-15 16:10:25 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/index.html'],
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        xl: '1170px'
+      }
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#217BF4',
+        heading: '#0A093D',
+        paragraph: '#656464'
+      },
+      boxShadow: {
+        primary: '0px 7px 22px -12px rgba(33, 123, 244, 0.34)',
+        thumbnail: '-5px 4px 14px rgba(151, 151, 151, 0.28)',
+        card: '0px 26px 49px rgba(146, 165, 184, 0.14)'
+      },
+      spacing: {
+        '4.5': '1.125rem', /* 18px */
+        '10.5': '2.625rem', /* 42px */
+        18: '4.5rem' /* 72px */
+      },
+      borderRadius: {
+        custom: '0.875rem', /* 14px */
+      }
+    },
+  },
+  plugins: [],
+}
